4ae72d5eb10ceb46f40eeb0f3e9e7e610fb0a331
Natalya Sharova <nat_sha10@mail.ru>
2026-02-22 18:49:20 +0300

    feature 30_1

---
+++ study/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from study.apps import StudyConfig
+from study.views import CourseViewSet, LessonListAPIView, LessonCreateAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
+
+app_name = StudyConfig.name
+
+router = SimpleRouter()
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path('lessons/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/update', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+]
+
+urlpatterns += router.urls
